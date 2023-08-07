bc8885a6022e32cf618fe4e1d77dfb698081245e
Aritra Das <97398407+AritraDasIT@users.noreply.github.com>
2023-08-07 18:13:27 +0530

    Create scientificCalculator.py

---
+++ scientificCalculator.py
@@ -0,0 +1,37 @@
+def addition(a,b):
+    c=a+b;
+    return c;
+    
+def substration(a,b):
+    c=a-b
+    return c;
+
+def multiplication(a,b):
+    c=a*b
+    return c;
+
+def division(a,b):
+    c=a/b;
+    return c;
+   
+   
+    
+    
+b=int(input("Enter the 1st number: "));
+c=int(input("Enter the 2nd number: "));
+
+Addition=addition(b,c);
+Substration=substration(b,c);
+Multiplication=multiplication(b,c);
+Division= division(b,c);
+
+
+print(Addition);
+print(Substration);
+print(Multiplication);
+print(Division);
+
+    
+    
+
+    
