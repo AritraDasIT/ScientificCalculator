8570b10140f4e599937e9dc06de460bbc1d8d272
Aritra Das <97398407+AritraDasIT@users.noreply.github.com>
2023-08-20 09:07:30 +0530

    Create Calculator.java

---
+++ Calculator.java
@@ -0,0 +1,36 @@
+
+import java.util.*;
+public class Main
+{
+	
+
+
+    public static void main(String args[])
+    {
+        int a , b , c ;
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Enter a : ");
+
+        a= scan.nextInt();
+
+        System.out.println("Enter b : ");
+
+        b=scan.nextInt();
+
+
+
+        System.out.println("Sum = " +(a+b));
+
+        System.out.println("Difference = " + (a-b));
+
+        System.out.println("Product = " + (a*b));
+
+        System.out.println("division= " +(a/b));
+
+
+    }
+}
+
+
